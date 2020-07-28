04098033519df177f6e98dd85a3eefd2af5a94ba
Emanuel Dorigon <emanueldorigon123@gmail.com>
2020-07-28 14:35:31 -0300

    Project initiated

---
+++ ExercicioTryCatch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
