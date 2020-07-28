23eadf649335e081e1d8e6e4321f76784ccec3da
Emanuel Dorigon <emanueldorigon123@gmail.com>
2020-07-28 14:54:48 -0300

    Project paused

---
+++ ExercicioTryCatch/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ExercicioTryCatch.Entities.Exceptions;
+
+namespace ExercicioTryCatch.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        { 
+        }
+    }
+}
